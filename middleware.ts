81613eb3480c6b690e8298a196b4a12da7ebf027
Harsh Sinha <harshsinha21may@gmail.com>
2025-06-02 20:02:00 +0530

    init

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { withAuth } from "next-auth/middleware"
 
 export default withAuth(
   function middleware(req) {
-    // Add any additional middleware logic here
+    
   },
   {
     callbacks: {
@@ -37,12 +37,7 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
+    
     "/((?!_next/static|_next/image|favicon.ico).*)",
   ],
 }
